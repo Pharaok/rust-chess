acf4de5e37526190a30a2469dcd752e9c9efb9d3
Pharaok <farouk.tamer03@gmail.com>
2022-12-30 03:08:50 +0200

    Move index and piece abbreviation conversion code to static methods.

--- src/board.rs
+++ src/board.rs
@@ -7,9 +7,29 @@ pub struct Board {
 
 impl Board {
     const START_FEN: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+    const PIECES: [char; 6] = ['P', 'N', 'B', 'R', 'Q', 'K'];
+
+    fn piece_to_index(c: char) -> Option<usize> {
+        Self::PIECES
+            .iter()
+            .position(|&p| p == c.to_ascii_uppercase())
+            .and_then(|p| Some((1 + p) * 2 + c.is_ascii_lowercase() as usize))
+    }
+    fn index_to_piece(i: usize) -> Option<char> {
+        if !i < (1 + Self::PIECES.len()) * 2 {
+            return None;
+        }
+        let mut piece = Self::PIECES[i / 2 - 1];
+        if i % 2 == 1 {
+            piece = piece.to_ascii_lowercase();
+        }
+        Some(piece)
+    }
 
     fn blank_board() -> Self {
-        Self { bitboards: [0; 14] }
+        Self {
+            bitboards: [0; (1 + Self::PIECES.len()) * 2],
+        }
     }
     pub fn new() -> Self {
         Self::from_fen(Self::START_FEN).unwrap()
@@ -29,25 +49,21 @@ impl Board {
         for rank in fen.split("/") {
             let mut file_index = 0;
             for c in rank.chars() {
-                let mut piece_type = c.is_ascii_lowercase() as usize;
+                if file_index > 7 {
+                    break;
+                }
+
                 if c.is_ascii_digit() {
                     file_index += c.to_digit(10).unwrap();
                     continue;
                 }
-                match c.to_ascii_uppercase() {
-                    'P' => piece_type += 2,
-                    'N' => piece_type += 4,
-                    'B' => piece_type += 6,
-                    'R' => piece_type += 8,
-                    'Q' => piece_type += 10,
-                    'K' => piece_type += 12,
-                    _ => return Err(()),
+
+                if let Some(piece_type) = Self::piece_to_index(c) {
+                    draft_board.bitboards[piece_type % 2] |= 1 << (rank_index * 8 + file_index);
+                    draft_board.bitboards[piece_type] |= 1 << (rank_index * 8 + file_index);
+                } else {
+                    return Err(());
                 }
-                if file_index > 7 {
-                    break;
-                }
-                draft_board.bitboards[piece_type % 2] |= 1 << (rank_index * 8 + file_index);
-                draft_board.bitboards[piece_type] |= 1 << (rank_index * 8 + file_index);
                 file_index += 1;
             }
             if rank_index == 0 {
@@ -91,20 +107,7 @@ impl Board {
 
                 a.push(' ');
                 if let Some(piece) = self.piece_at(square) {
-                    let mut c;
-                    match piece / 2 {
-                        1 => c = 'P',
-                        2 => c = 'N',
-                        3 => c = 'B',
-                        4 => c = 'R',
-                        5 => c = 'Q',
-                        6 => c = 'K',
-                        _ => panic!(),
-                    }
-                    if piece % 2 == 1 {
-                        c = c.to_ascii_lowercase();
-                    }
-                    a.push(c);
+                    a.push(Self::index_to_piece(piece).unwrap());
                 } else {
                     a.push('.')
                 }
